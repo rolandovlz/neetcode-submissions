b24471fb75665d587402523dc69c2fc72c1804e7
Rolando Velez <rolandovlz@gmail.com>
2026-07-19 12:53:04 -0700

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const map = new Map()
+        for (const str of strs) {
+            const sorted = str.split('').sort().join('')
+            if (!map.has(sorted)) {
+                map.set(sorted, [])
+            }
+            map.get(sorted).push(str)
+        }
+        return [...map.values()]
+    }
+}
+
+
